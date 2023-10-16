67c8e93e0433cf21e630677ea3026e01568aea4e
Jay Nwanze <jay.nwanze@sap.com>
2023-10-17 00:26:52 +0100

    Intial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from tradingBot import runTradingBot  # Import the function from tradingBot.py
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/run_script", methods=["POST"])
+def run_script():
+    try:
+        # Retrieve the cryptocurrency name and symbol from the form
+        # cryptocurrency_name = request.form["cryptocurrency_name"]
+        # cryptocurrency_symbol_pair = request.form["cryptocurrency_symbol_pair"]
+
+        # Run trading bot and save script output
+        script_output = runTradingBot()
+
+        # render output from  as html page
+        return render_template("result.html", script_output=script_output)
+    except Exception as e:
+        return render_template({"error": f"Script execution failed: {str(e)}"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
